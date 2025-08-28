3a0616ed9fd94769ea1def4443fff13bac980d61
jzcurious <paranoid.hpc.io@gmail.com>
2025-08-28 12:27:21 +0700

    change Item

--- staticmap/include/staticmap/item.hpp
+++ staticmap/include/staticmap/item.hpp
@@ -3,24 +3,32 @@
 
 #include <concepts>
 
-namespace tmap {
+namespace smap {
+
+template <class T>
+concept Comparable = requires(T x, T y) {
+  { x == y } -> std::convertible_to<bool>;
+};
 
 template <class T>
 concept ItemKind = requires {
   typename T::key_t;
   typename T::val_t;
+  Comparable<typename T::key_t>;
+  { T::key } -> std::convertible_to<typename T::key_t>;
 } and requires(T x) {
-  { x.val } -> std::convertible_to<typename T::val_t>;
+  { x.val } -> std::same_as<typename T::val_t&>;
 };
 
-template <class KeyT, class ValT>
+template <Comparable auto _key, class ValT>
 struct Item {
-  using key_t = KeyT;
+  using key_t = decltype(_key);
   using val_t = ValT;
 
+  static constexpr const key_t key = _key;
   val_t val;
 };
 
-}  // namespace tmap
+}  // namespace smap
 
 #endif  // STATICMAP_ITEM_HPP
